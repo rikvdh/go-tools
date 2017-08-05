54c3669e123aa9dc8dfb2a25629c879d3124aef2
Rik van der Heijden <mail@rikvanderheijden.com>
2017-08-05 17:31:15 +0200

    Stub brightness on Darwin

---
+++ lib/brightness/brightness_darwin.go
@@ -0,0 +1,15 @@
+// Package brightness controls brightness for backlight-enabled systems
+package brightness
+
+func (b *Brightness) init() error {
+	return nil
+}
+
+// Get returns the brightness for
+func (b *Brightness) Get() float64 {
+	return 0
+}
+
+// Set sets the brightness
+func (b *Brightness) Set(newBrightness float64) {
+}
